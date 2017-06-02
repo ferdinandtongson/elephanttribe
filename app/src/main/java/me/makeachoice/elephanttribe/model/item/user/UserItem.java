cf25c134fc1f657a08c7ec13da925b2f84386af4
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-06-02 12:17:38 -0700

    Add firebase constructor option

--- app/src/main/java/me/makeachoice/elephanttribe/model/item/user/UserItem.java
+++ app/src/main/java/me/makeachoice/elephanttribe/model/item/user/UserItem.java
@@ -3,7 +3,10 @@ package me.makeachoice.elephanttribe.model.item.user;
 import android.content.ContentValues;
 import android.database.Cursor;
 
+import com.google.firebase.auth.FirebaseUser;
+
 import me.makeachoice.elephanttribe.model.contract.user.UserContract;
+import me.makeachoice.elephanttribe.utilities.DateTimeUtility;
 
 /**
  * UserItem holds basic user registration data
@@ -29,19 +32,30 @@ public class UserItem extends _UserBaseItem {
  */
 /**************************************************************************************************/
 
-    public UserItem(){}
+    public UserItem(){
+        userId = "";
+        userName = "";
+        email = "";
+        registration = "";
+    }
 
     public UserItem(_UserBaseItem item){
-        user = item.user;
+        userName = item.userName;
         email = item.email;
         registration = item.registration;
     }
 
+    public UserItem(FirebaseUser fbUser){
+        userId = fbUser.getUid();
+        userName = fbUser.getDisplayName();
+        email = fbUser.getEmail();
+        registration = DateTimeUtility.getToday();
+    }
 
     public UserItem(Cursor cursor){
         _id = cursor.getLong(UserContract.INDEX_ID);
         userId = cursor.getString(UserContract.INDEX_USERID);
-        user = cursor.getString(UserContract.INDEX_USER);
+        userName = cursor.getString(UserContract.INDEX_USER);
         email = cursor.getString(UserContract.INDEX_EMAIL);
         registration = cursor.getString(UserContract.INDEX_REGISTRATION);
     }
@@ -60,17 +74,17 @@ public class UserItem extends _UserBaseItem {
     public ContentValues getContentValues(){
         //create content value
         ContentValues values = new ContentValues();
-        values.put(UserContract.COLUMN_USERID, userId);
-        values.put(UserContract.COLUMN_USER, user);
-        values.put(UserContract.COLUMN_EMAIL, email);
-        values.put(UserContract.COLUMN_REGISTRATION, registration);
+        values.put(UserContract.USERID, userId);
+        values.put(UserContract.USERNAME, userName);
+        values.put(UserContract.EMAIL, email);
+        values.put(UserContract.REGISTRATION, registration);
 
         return values;
     }
 
     public _UserBaseItem getFBItem(){
         _UserBaseItem fbItem = new _UserBaseItem();
-        fbItem.user = user;
+        fbItem.userName = userName;
         fbItem.email = email;
         fbItem.registration = registration;
 
