3ee79e31c15ebdf388aa45534a4b6c1655ce6080
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-06-03 08:21:17 -0700

    Add Deck Detail server names

--- app/src/main/java/me/makeachoice/elephanttribe/controller/manager/base/iServerName.java
+++ app/src/main/java/me/makeachoice/elephanttribe/controller/manager/base/iServerName.java
@@ -13,6 +13,7 @@ public interface iServerName {
 /**************************************************************************************************/
 
     final static String KEEPER_DECK = "kDeck";
+    final static String KEEPER_DECK_DETAIL = "kDeckDetail";
     final static String KEEPER_FLASHCARD = "kFlashcard";
 
     final static String KEEPER_TUTORIAL = "kTutorial";
@@ -26,6 +27,12 @@ public interface iServerName {
  * Maid Names:
  */
 /**************************************************************************************************/
+
+    //deck detail maids
+    final static String MAID_DECK_DETAIL_INFO = "mDeckDetailInfo";
+    final static String MAID_DECK_DETAIL_SCHOOL = "mDeckDetailSchool";
+    final static String MAID_DECK_DETAIL_FLASHCARD = "mDeckDetailFlashcard";
+
 
     //flashcard maids
     final static String MAID_FLASHCARD_SIMPLE = "mFlashcardSimple";
