b11b5d7750c5f6f57b7763ab87a846c8a12e4c86
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-06-02 17:37:54 -0700

    Add flashcard server names

--- app/src/main/java/me/makeachoice/elephanttribe/controller/manager/base/iServerName.java
+++ app/src/main/java/me/makeachoice/elephanttribe/controller/manager/base/iServerName.java
@@ -13,6 +13,8 @@ public interface iServerName {
 /**************************************************************************************************/
 
     final static String KEEPER_DECK = "kDeck";
+    final static String KEEPER_FLASHCARD = "kFlashcard";
+
     final static String KEEPER_TUTORIAL = "kTutorial";
     final static String KEEPER_MARKET = "kMarket";
     final static String KEEPER_SETTINGS = "kSettings";
@@ -24,6 +26,9 @@ public interface iServerName {
  * Maid Names:
  */
 /**************************************************************************************************/
+
+    //flashcard maids
+    final static String MAID_FLASHCARD_SIMPLE = "mFlashcardSimple";
 
 
 /**************************************************************************************************/
