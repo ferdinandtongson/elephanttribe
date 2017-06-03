959b842e7a7790c555fb7a86e881ab2ae7cb5449
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-06-02 17:41:38 -0700

    first commit

---
+++ app/src/main/java/me/makeachoice/elephanttribe/model/item/flashcard/_FlashcardBaseItem.java
@@ -0,0 +1,14 @@
+package me.makeachoice.elephanttribe.model.item.flashcard;
+
+/**
+ * Firebase flashcard item object
+ */
+
+public class _FlashcardBaseItem {
+
+    //flashcard --> userId --> deckId --> cardId
+    public String card;
+    public String answer;
+    public String picture;
+    public String voice;
+}
