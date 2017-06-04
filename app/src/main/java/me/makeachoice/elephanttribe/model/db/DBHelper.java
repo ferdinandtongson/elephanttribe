ad30d6cb6ee3373415abe615fcbf5c1d63ab1a99
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-06-04 12:26:59 -0700

    Add deck tag table

--- app/src/main/java/me/makeachoice/elephanttribe/model/db/DBHelper.java
+++ app/src/main/java/me/makeachoice/elephanttribe/model/db/DBHelper.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import me.makeachoice.elephanttribe.model.contract.deck.DeckContract;
+import me.makeachoice.elephanttribe.model.contract.deck.DeckTagContract;
 import me.makeachoice.elephanttribe.model.contract.flashcard.FlashcardContract;
 import me.makeachoice.elephanttribe.model.contract.user.UserContract;
 
@@ -58,6 +59,7 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sql){
         sql.execSQL(UserContract.CREATE_TABLE);
         sql.execSQL(DeckContract.CREATE_TABLE);
+        sql.execSQL(DeckTagContract.CREATE_TABLE);
         sql.execSQL(FlashcardContract.CREATE_TABLE);
     }
 
@@ -68,6 +70,7 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sql, int oldVersion, int newVersion){
         dropTable(sql, UserContract.TABLE_NAME);
         dropTable(sql, DeckContract.TABLE_NAME);
+        dropTable(sql, DeckTagContract.TABLE_NAME);
         dropTable(sql, FlashcardContract.TABLE_NAME);
 
         onCreate(sql);
