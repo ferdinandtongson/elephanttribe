4bef514f801ee788b8177b35b680acd02c30bf29
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-06-04 12:27:48 -0700

    first commit

---
+++ app/src/main/java/me/makeachoice/elephanttribe/model/item/deck/_DeckTagBaseItem.java
@@ -0,0 +1,19 @@
+package me.makeachoice.elephanttribe.model.item.deck;
+
+/**
+ * Created by tongson on 4/29/17.
+ */
+
+public class _DeckTagBaseItem {
+
+/**************************************************************************************************/
+/*
+ * _DeckTagBaseItem is user deck tag data for Firebase database
+ */
+/**************************************************************************************************/
+
+    public String tag;
+
+/**************************************************************************************************/
+
+}
